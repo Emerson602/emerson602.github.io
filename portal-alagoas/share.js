10b3a94d792fafafc909a980231eabce5dccc8f1
Emerson602 <48162463+Emerson602@users.noreply.github.com>
2022-05-16 12:44:40 -0300

    Add files via upload

---
+++ portal-alagoas/share.js
@@ -0,0 +1,390 @@
+var btn = document.querySelector("#btn");
+var btn2 = document.querySelector("#btn2");
+  
+  
+  btn.addEventListener("click", function translate() {
+    var div = document.querySelector("#tx1_translate2");
+    
+  if(div.style.display === "none") {
+        div.style.display = "flex";
+        document.querySelector("#tx1_translate").style.display = "none";
+        document.querySelector("#tx1_translate3").style.display = "none";
+        //porthoguese
+        document.getElementById('btn').src ='imgs/brasil.webp';
+        //english
+        document.getElementById('btn2').src = 'imgs/united.webp';
+
+    } else {
+      div.style.display = "none";
+      document.querySelector("#tx1_translate").style.display = "flex";
+      //italian
+      document.getElementById('btn').src ='imgs/italy.webp';  
+      // english
+      document.getElementById('btn2').src = 'imgs/united.webp';     
+
+  }
+    
+});
+
+
+var div2 = document.querySelector("#tx1_translate2").style.display = "none"; 
+
+/*===========================================================*/
+
+
+
+
+
+
+
+
+btn2.addEventListener("click", function translate() {
+    var div = document.querySelector("#tx1_translate3");
+    
+  if(div.style.display === "none") {
+        div.style.display = "flex";
+        document.querySelector("#tx1_translate2").style.display = "none";
+        document.querySelector("#tx1_translate").style.display = "none";
+        //italian
+        document.getElementById('btn').src = 'imgs/italy.webp';
+        //portoghese
+        document.getElementById('btn2').src = 'imgs/brasil.webp';
+
+    } else {
+      div.style.display = "none";
+      document.querySelector("#tx1_translate").style.display = "flex";
+      //italian
+      document.getElementById('btn').src = 'imgs/italy.webp';
+      //english
+      document.getElementById('btn2').src = 'imgs/brasil.webp';
+
+
+  }
+    
+});
+
+
+var div2 = document.querySelector("#tx1_translate3").style.display = "none"; 
+
+
+/*===================================translate-2=========================================*/
+
+
+
+btn.addEventListener("click", function translate2() {
+    var div = document.querySelector("#tx2_translate2");
+    var div = document.querySelector("#tx2_translate2"); 
+    
+  if(div.style.display === "none") {
+        div.style.display = "flex";
+        document.querySelector("#tx2_translate").style.display = "none";
+        document.querySelector("#tx2_translate3").style.display = "none";
+        //porthoguese
+        document.getElementById('btn').src ='imgs/brasil.webp';
+        //english
+        document.getElementById('btn2').src = 'imgs/united.webp';
+
+    } else {
+      div.style.display = "none";
+      document.querySelector("#tx2_translate").style.display = "flex";
+      //italian
+      document.getElementById('btn').src ='imgs/italy.webp';  
+      // english
+      document.getElementById('btn2').src = 'imgs/united.webp';     
+
+  }
+    
+});
+
+
+var div2 = document.querySelector("#tx2_translate2").style.display = "none"; 
+
+/*===========================================================*/
+
+
+btn2.addEventListener("click", function translate2() {
+    var div = document.querySelector("#tx2_translate3");
+    
+  if(div.style.display === "none") {
+        div.style.display = "flex";
+        document.querySelector("#tx2_translate2").style.display = "none";
+        document.querySelector("#tx2_translate").style.display = "none";
+        //italian
+        document.getElementById('btn').src = 'imgs/italy.webp';
+        //portoghese
+        document.getElementById('btn2').src = 'imgs/brasil.webp';
+
+    } else {
+      div.style.display = "none";
+      document.querySelector("#tx2_translate").style.display = "flex";
+      //italian
+      document.getElementById('btn').src = 'imgs/italy.webp';
+      //english
+      document.getElementById('btn2').src = 'imgs/brasil.webp';
+
+
+  }
+    
+});
+
+
+var div2 = document.querySelector("#tx2_translate3").style.display = "none"; 
+
+
+
+
+/*=====================================translate-3===================================*/
+
+
+
+
+btn.addEventListener("click", function translate3() {
+    var div = document.querySelector("#tx3_translate2");
+    
+  if(div.style.display === "none") {
+        div.style.display = "flex";
+        document.querySelector("#tx3_translate").style.display = "none";
+        document.querySelector("#tx3_translate3").style.display = "none";
+        //porthoguese
+        document.getElementById('btn').src ='imgs/brasil.webp';
+        //english
+        document.getElementById('btn2').src = 'imgs/united.webp';
+
+    } else {
+      div.style.display = "none";
+      document.querySelector("#tx3_translate").style.display = "flex";
+      //italian
+      document.getElementById('btn').src ='imgs/italy.webp';  
+      // english
+      document.getElementById('btn2').src = 'imgs/united.webp';     
+
+  }
+    
+});
+
+
+var div2 = document.querySelector("#tx3_translate2").style.display = "none"; 
+
+/*===========================================================*/
+
+
+btn2.addEventListener("click", function translate3() {
+    var div = document.querySelector("#tx3_translate3");
+    
+  if(div.style.display === "none") {
+        div.style.display = "flex";
+        document.querySelector("#tx3_translate2").style.display = "none";
+        document.querySelector("#tx3_translate").style.display = "none";
+        //italian
+        document.getElementById('btn').src = 'imgs/italy.webp';
+        //portoghese
+        document.getElementById('btn2').src = 'imgs/brasil.webp';
+
+    } else {
+      div.style.display = "none";
+      document.querySelector("#tx3_translate").style.display = "flex";
+      //italian
+      document.getElementById('btn').src = 'imgs/italy.webp';
+      //english
+      document.getElementById('btn2').src = 'imgs/brasil.webp';
+
+
+  }
+    
+});
+
+
+var div2 = document.querySelector("#tx3_translate3").style.display = "none"; 
+
+/*=====================================translate-4===================================*/
+
+
+
+
+btn.addEventListener("click", function translate4() {
+    var div = document.querySelector("#tx4_translate2");
+    
+  if(div.style.display === "none") {
+        div.style.display = "flex";
+        document.querySelector("#tx4_translate").style.display = "none";
+        document.querySelector("#tx4_translate3").style.display = "none";
+        //porthoguese
+        document.getElementById('btn').src ='imgs/brasil.webp';
+        //english
+        document.getElementById('btn2').src = 'imgs/united.webp';
+
+    } else {
+      div.style.display = "none";
+      document.querySelector("#tx4_translate").style.display = "flex";
+      //italian
+      document.getElementById('btn').src ='imgs/italy.webp';  
+      // english
+      document.getElementById('btn2').src = 'imgs/united.webp';     
+
+  }
+    
+});
+
+
+var div2 = document.querySelector("#tx4_translate2").style.display = "none"; 
+
+/*===========================================================*/
+
+
+btn2.addEventListener("click", function translate4() {
+    var div = document.querySelector("#tx4_translate3");
+    
+  if(div.style.display === "none") {
+        div.style.display = "flex";
+        document.querySelector("#tx4_translate2").style.display = "none";
+        document.querySelector("#tx4_translate").style.display = "none";
+        //italian
+        document.getElementById('btn').src = 'imgs/italy.webp';
+        //portoghese
+        document.getElementById('btn2').src = 'imgs/brasil.webp';
+
+    } else {
+      div.style.display = "none";
+      document.querySelector("#tx4_translate").style.display = "flex";
+      //italian
+      document.getElementById('btn').src = 'imgs/italy.webp';
+      //english
+      document.getElementById('btn2').src = 'imgs/brasil.webp';
+
+
+  }
+    
+});
+
+
+var div2 = document.querySelector("#tx4_translate3").style.display = "none"; 
+
+
+
+/*=====================================translate-5===================================*/
+
+
+
+
+btn.addEventListener("click", function translate5() {
+    var div = document.querySelector("#tx5_translate2");
+    
+  if(div.style.display === "none") {
+        div.style.display = "flex";
+        document.querySelector("#tx5_translate").style.display = "none";
+        document.querySelector("#tx5_translate3").style.display = "none";
+        //porthoguese
+        document.getElementById('btn').src ='imgs/brasil.webp';
+        //english
+        document.getElementById('btn2').src = 'imgs/united.webp';
+
+    } else {
+      div.style.display = "none";
+      document.querySelector("#tx5_translate").style.display = "flex";
+      //italian
+      document.getElementById('btn').src ='imgs/italy.webp';  
+      // english
+      document.getElementById('btn2').src = 'imgs/united.webp';     
+
+  }
+    
+});
+
+
+var div2 = document.querySelector("#tx5_translate2").style.display = "none"; 
+
+/*===========================================================*/
+
+
+btn2.addEventListener("click", function translate5() {
+    var div = document.querySelector("#tx5_translate3");
+    
+  if(div.style.display === "none") {
+        div.style.display = "flex";
+        document.querySelector("#tx5_translate2").style.display = "none";
+        document.querySelector("#tx5_translate").style.display = "none";
+        //italian
+        document.getElementById('btn').src = 'imgs/italy.webp';
+        //portoghese
+        document.getElementById('btn2').src = 'imgs/brasil.webp';
+
+    } else {
+      div.style.display = "none";
+      document.querySelector("#tx5_translate").style.display = "flex";
+      //italian
+      document.getElementById('btn').src = 'imgs/italy.webp';
+      //english
+      document.getElementById('btn2').src = 'imgs/brasil.webp';
+
+
+  }
+    
+});
+
+
+var div2 = document.querySelector("#tx5_translate3").style.display = "none"; 
+
+
+
+/*=====================================translate-6===================================*/
+
+
+
+
+btn.addEventListener("click", function translate6() {
+    var div = document.querySelector("#tx6_translate2");
+    
+  if(div.style.display === "none") {
+        div.style.display = "flex";
+        document.querySelector("#tx6_translate").style.display = "none";
+        document.querySelector("#tx6_translate3").style.display = "none";
+        //porthoguese
+        document.getElementById('btn').src ='imgs/brasil.webp';
+        //english
+        document.getElementById('btn2').src = 'imgs/united.webp';
+
+    } else {
+      div.style.display = "none";
+      document.querySelector("#tx6_translate").style.display = "flex";
+      //italian
+      document.getElementById('btn').src ='imgs/italy.webp';  
+      // english
+      document.getElementById('btn2').src = 'imgs/united.webp';     
+
+  }
+    
+});
+
+
+var div2 = document.querySelector("#tx6_translate2").style.display = "none"; 
+
+/*===========================================================*/
+
+
+btn2.addEventListener("click", function translate6() {
+    var div = document.querySelector("#tx6_translate3");
+    
+  if(div.style.display === "none") {
+        div.style.display = "flex";
+        document.querySelector("#tx6_translate2").style.display = "none";
+        document.querySelector("#tx6_translate").style.display = "none";
+        //italian
+        document.getElementById('btn').src = 'imgs/italy.webp';
+        //portoghese
+        document.getElementById('btn2').src = 'imgs/brasil.webp';
+
+    } else {
+      div.style.display = "none";
+      document.querySelector("#tx6_translate").style.display = "flex";
+      //italian
+      document.getElementById('btn').src = 'imgs/italy.webp';
+      //english
+      document.getElementById('btn2').src = 'imgs/brasil.webp';
+
+
+  }
+    
+});
+
+
+var div2 = document.querySelector("#tx6_translate3").style.display = "none"; 
