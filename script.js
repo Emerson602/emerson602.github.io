f2e04e0ee46da0e9e282a52ede0d33c96ba9106a
Emerson Nicacio <48162463+Emerson602@users.noreply.github.com>
2022-07-19 00:18:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 	let words = ["Hello world!", "I'm a...","Web developer."];
 
+
+
+
+
+
 function transL(){
 
 				 let btn = document.getElementById("btnImg"); 		
@@ -17,8 +22,8 @@ function transL(){
 				 let hm = document.getElementById("hire");
 				 let rd = document.getElementById("right");
 				 let rm = document.getElementById("right2"); 
-				 let btnGal = document.getElementById("btn-galery"); 
-			
+				 let btnGal = document.getElementById("btn-gallery"); 
+				
 		
 			
 					  if (
@@ -36,13 +41,12 @@ function transL(){
 					    hmb.innerHTML = "Home";
 					    amb.innerHTML = "About me";
 					    smb.innerHTML = "Skills";
-					    cmb.innerHTML = "Contact";
-					
+					    cmb.innerHTML = "Contact";						
 					    rd.innerHTML = "© All right reserved.";
-					    rm.innerHTML = "© All right reserved."; 
-					
+					    rm.innerHTML = "© All right reserved."; 						
 					    hm.innerHTML = "Hire me";
 					    words = ["Hello world!", "I'm a...","Web developer."];
+					    btnGal.innerHTML = "Live Preview"
 					    			    	
 					  					
 									    
@@ -59,10 +63,10 @@ function transL(){
 					    smb.innerHTML = "Habilidades";	
 					    cmb.innerHTML = "Contato";					    
 					    rd.innerHTML = "© Todos os direitos reservados.";
-					    rm.innerHTML = "© Todos os direitos reservados.";
-					
+					    rm.innerHTML = "© Todos os direitos reservados.";						
 					    hm.innerHTML = "Contrate-me";	
-					    words = ["Olá mundo!", "Eu sou...", "Desenvolvedor web."];  
+					    words = ["Olá mundo!", "Eu sou...", "Desenvolvedor web."]; 
+					    btnGal.innerHTML = "Visualizar" 
 
 					  } 
 					}
@@ -105,4 +109,251 @@ function deletingEffect() {
 	loopDeleting();
 };
 
-typingEffect();
+typingEffect(); 
+
+
+/*about me*/
+
+
+function transL2(){
+
+				 let btn = document.getElementById("btnImg"); 		
+				 let tt = document.getElementById("tt-transl");	
+				 let h = document.getElementById("home");
+				 let a = document.getElementById("about");				 
+				 let s = document.getElementById("skills");
+				 let c = document.getElementById("contact");
+
+				 let hmb = document.getElementById("homeMobi");
+				 let amb = document.getElementById("aboutMobi");				 
+				 let smb = document.getElementById("skillsMobi");
+				 let cmb = document.getElementById("contactMobi");
+
+				 
+				 let rd = document.getElementById("right");
+				 let rm = document.getElementById("right2");
+				 let tts = document.getElementById("tt-sobre");
+				 let tx = document.getElementById("tx-sobre"); 
+				 
+				
+		
+			
+					  if (
+
+					  	h.innerHTML === "Inicio"					  	
+					  	){
+
+						
+					    btn.src ='pics/portugal.webp';
+					    tt.innerHTML = "Traduzir:";
+					    h.innerHTML = "Home";
+					    a.innerHTML = "About me";
+					    s.innerHTML = "Skills";
+					    c.innerHTML = "Contact";
+					    hmb.innerHTML = "Home";
+					    amb.innerHTML = "About me";
+					    smb.innerHTML = "Skills";
+					    cmb.innerHTML = "Contact";						
+					    rd.innerHTML = "© All right reserved.";
+					    rm.innerHTML = "© All right reserved.";
+					    tts.innerHTML = "Know Me More"; 
+					    tx.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."; 		 						
+					   
+					    			    	
+					  					
+									    
+					  } else {
+
+						btn.src ='pics/united.webp';
+					    tt.innerHTML = "Translate: ";
+					    h.innerHTML = "Inicio";
+					    a.innerHTML = "Sobre";
+					    s.innerHTML = "Habilidades";
+					    c.innerHTML = "Contato";
+					    hmb.innerHTML = "Inicio";
+					    amb.innerHTML = "Sobre";
+					    smb.innerHTML = "Habilidades";	
+					    cmb.innerHTML = "Contato";					    
+					    rd.innerHTML = "© Todos os direitos reservados.";
+					    rm.innerHTML = "© Todos os direitos reservados.";
+					    tts.innerHTML = "Quem sou"; 
+					    tx.innerHTML = "texto em construção!"; 		 												
+					   
+
+					  } 
+					}
+
+
+/*portfolio*/
+
+
+
+function transL3(){
+
+				 let btn = document.getElementById("btnImg"); 		
+				 let tt = document.getElementById("tt-transl");	
+				 let h = document.getElementById("home");
+				 let a = document.getElementById("about");				 
+				 let s = document.getElementById("skills");
+				 let c = document.getElementById("contact");
+
+				 let hmb = document.getElementById("homeMobi");
+				 let amb = document.getElementById("aboutMobi");				 
+				 let smb = document.getElementById("skillsMobi");
+				 let cmb = document.getElementById("contactMobi");
+
+				 let vz1 = document.getElementById("btn-galery-1");
+				 let vz2 = document.getElementById("btn-galery-2");
+				 let vz3 = document.getElementById("btn-galery-3");
+				 let vz4 = document.getElementById("btn-galery-4");
+
+				 let rd = document.getElementById("right");
+				 let rm = document.getElementById("right2");
+				
+				 
+				
+		
+			
+					  if (
+
+					  	h.innerHTML === "Inicio"					  	
+					  	){
+
+						
+					    btn.src ='pics/portugal.webp';
+					    tt.innerHTML = "Traduzir:";
+					    h.innerHTML = "Home";
+					    a.innerHTML = "About me";
+					    s.innerHTML = "Skills";
+					    c.innerHTML = "Contact";
+					    hmb.innerHTML = "Home";
+					    amb.innerHTML = "About me";
+					    smb.innerHTML = "Skills";
+					    cmb.innerHTML = "Contact";
+
+					    vz1.innerHTML = "Live preview";	
+					    vz2.innerHTML = "Live preview";	
+					    vz3.innerHTML = "Live preview";	
+					    vz4.innerHTML = "Live preview";	
+
+					    rd.innerHTML = "© All right reserved.";
+					    rm.innerHTML = "© All right reserved.";
+					  	
+
+					    
+					    
+					   
+					    			    	
+					  					
+									    
+					  } else {
+
+						btn.src ='pics/united.webp';
+					    tt.innerHTML = "Translate: ";
+					    h.innerHTML = "Inicio";
+					    a.innerHTML = "Sobre";
+					    s.innerHTML = "Habilidades";
+					    c.innerHTML = "Contato";
+					    hmb.innerHTML = "Inicio";
+					    amb.innerHTML = "Sobre";
+					    smb.innerHTML = "Habilidades";	
+					    cmb.innerHTML = "Contato";
+
+					    vz1.innerHTML = "Visualizar";
+					    vz2.innerHTML = "Visualizar";
+					    vz3.innerHTML = "Visualizar";
+					    vz4.innerHTML = "Visualizar";
+
+					    rd.innerHTML = "© Todos os direitos reservados.";
+					    rm.innerHTML = "© Todos os direitos reservados.";
+					       
+					 
+					  } 
+					} 
+
+
+
+/*skills*/	
+
+
+
+
+
+function transL4(){
+
+				 let btn = document.getElementById("btnImg"); 		
+				 let tt = document.getElementById("tt-transl");	
+				 let h = document.getElementById("home");
+				 let a = document.getElementById("about");				 
+				 let s = document.getElementById("skills");
+				 let c = document.getElementById("contact");
+
+				 let hmb = document.getElementById("homeMobi");
+				 let amb = document.getElementById("aboutMobi");				 
+				 let smb = document.getElementById("skillsMobi");
+				 let cmb = document.getElementById("contactMobi");
+
+				 let sp = document.getElementById("my-skills");	
+
+				 let rd = document.getElementById("right");
+				 let rm = document.getElementById("right2");
+				
+				 
+				
+		
+			
+					  if (
+
+					  	h.innerHTML === "Inicio"					  	
+					  	){
+
+						
+					    btn.src ='pics/portugal.webp';
+					    tt.innerHTML = "Traduzir:";
+					    h.innerHTML = "Home";
+					    a.innerHTML = "About me";
+					    s.innerHTML = "Skills";
+					    c.innerHTML = "Contact";
+					    hmb.innerHTML = "Home";
+					    amb.innerHTML = "About me";
+					    smb.innerHTML = "Skills";
+					    cmb.innerHTML = "Contact";
+
+					    sp.innerHTML = "My skills";	
+					   
+
+					    rd.innerHTML = "© All right reserved.";
+					    rm.innerHTML = "© All right reserved.";
+					  	
+
+					    
+					    
+					   
+					    			    	
+					  					
+									    
+					  } else {
+
+						btn.src ='pics/united.webp';
+					    tt.innerHTML = "Translate: ";
+					    h.innerHTML = "Inicio";
+					    a.innerHTML = "Sobre";
+					    s.innerHTML = "Habilidades";
+					    c.innerHTML = "Contato";
+					    hmb.innerHTML = "Inicio";
+					    amb.innerHTML = "Sobre";
+					    smb.innerHTML = "Habilidades";	
+					    cmb.innerHTML = "Contato";
+
+					    sp.innerHTML = "Minhas habilidades";	
+
+					    rd.innerHTML = "© Todos os direitos reservados.";
+					    rm.innerHTML = "© Todos os direitos reservados.";
+					       
+					 
+					  } 
+					} 
+
+
+
+
