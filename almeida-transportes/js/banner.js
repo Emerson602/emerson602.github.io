561db24928a3445688d6753356c2d4f6567716a3
Emerson Nicacio <48162463+Emerson602@users.noreply.github.com>
2023-09-02 17:24:27 -0300

    Add files via upload

---
+++ almeida-transportes/js/banner.js
@@ -0,0 +1,6 @@
+VanillaTilt.init(document.getElementById("banner"), {
+	max: 10,
+	speed: 400,
+	glare: true,
+	"max-glare": 1,
+});
---
+++ almeida-transportes/js/reveal.js
@@ -0,0 +1,44 @@
+window.sr = ScrollReveal({ reset: true });
+
+//sr.reveal('.sub-container-info', {duration: 2000});
+
+sr.reveal('.caracterist-imgs', {
+			rotate: { x:0, y: 80, z: 0},duration:2000
+		});  
+
+
+sr.reveal('.sub-satisfacao', {
+			rotate: { x:0, y: 80, z: 0},duration:2000
+		});  
+
+
+sr.reveal('.img-info', {duration: 2000});
+
+sr.reveal('.text-info', {duration: 2000});
+
+sr.reveal('.img-galeria', {duration: 2000}); 
+
+sr.reveal('.img-sobre', {duration: 2000}); 
+
+sr.reveal('.txt-sobre', {
+			rotate: { x:0, y: 80, z: 0},duration:2000
+		});  
+
+
+
+sr.reveal('.txt-serv-1', {
+			rotate: { x:0, y: 80, z: 0},duration:2000
+		});
+sr.reveal('.txt-serv-2', {
+			rotate: { x:0, y: 80, z: 0},duration:2000
+		}); 
+sr.reveal('.txt-serv-3', {
+			rotate: { x:0, y: 80, z: 0},duration:2000
+		});		 
+
+sr.reveal('.img-serv-1', {duration: 2000}); 
+
+sr.reveal('.img-serv-2', {duration: 2000});
+
+sr.reveal('.img-serv-3', {duration: 2000});
+
