ce2f2ce4c996e58cdd450e1a98e54945af22f398
Emerson Nicacio <48162463+Emerson602@users.noreply.github.com>
2024-01-05 17:49:27 -0300

    Add files via upload

--- financial-control/app.js
+++ financial-control/app.js
@@ -13,7 +13,9 @@ const app = {
             financialExpenses: localStorage.getItem("financialExpenses") || 0, 
             amount: parseInt(),
             currentDateTime: new Date(), 
-            numberTransaction: parseInt(localStorage.getItem("number-transaction") || 0),                         
+            numberTransaction: parseInt(localStorage.getItem("number-transaction") || 0),  
+            applyColor: 'success',
+            historyType: ''                                           
         }
     },
     methods: {
@@ -24,7 +26,7 @@ const app = {
                   const formattedDateTime = this.currentDateTime.toLocaleDateString('pt-BR', options);                                         
                   localStorage.setItem("dateTime", formattedDateTime);
                   this.dateTime = localStorage.getItem("dateTime")                                 
-            }, 10000) 
+            }, 1000) 
          }, 
         setCurrentYear() {            
             const footer = document.querySelector('#footer');   
@@ -39,7 +41,7 @@ const app = {
             this.reload()
         },  
         toggleHistoric() {
-            this.showHistoric = !this.showHistoric;                
+            this.showHistoric = !this.showHistoric;                                     
         },
         closeHistoric() {
             this.toggleHistoric()
@@ -48,28 +50,51 @@ const app = {
         
         getTransactionHistory() {
            this.transactionList = [];
+
             let count = 0;
             for (let i = 0; i < this.numberTransaction; i++) {
                 count += 1;
-                
+                 
                 const transactionDate = localStorage.getItem(`transaction-date-${count}`, this.dateTime);
                 const transactionDescription = localStorage.getItem(`transaction-description-${count}`, this.descriptionInput);
                 const transactionType = localStorage.getItem(`transaction-type-${count}`, this.typeInput);
-                const transactionValue = localStorage.getItem(`transaction-value-${count}`, this.valueInput);
-                                
-                this.transactionList.push(`      
-                    <span id="data" class="text-center">${transactionDate}</span>                         
-                    <span class="mt-2 text-start text-sm-center d-block">Descrição: <br>${transactionDescription}</span>
-                    <span class="mt-2 text-start text-sm-center d-block">Tipo: ${transactionType}</span>                
-                    <span class="text-start text-sm-center d-block">Valor: ${transactionValue}</span>                                           
-                `); 
+                const transactionValue = localStorage.getItem(`transaction-value-${count}`, this.valueInput);              
+                
+                this.historyType = transactionType;
 
+                this.setColor();
+   
+                this.transactionList.push(`  
+                  <div class="d-flex flex-column align-items-center justify-content-center bg-light p-3 border-5 border-start-0 border-start-top border-bottom-0 border-top-0 rounded mt-2  border border-${this.applyColor}">    
+                    
+                    <span id="data" class="text-center">${transactionDate}</span>   
+                    <span class="mt-2">Descrição: </span>                      
+                    <span class="mt-1 text-start text-sm-center fw-bold">${transactionDescription}</span>
+                    <span class="mt-2 text-start text-sm-center">Tipo: ${transactionType}</span> 
+
+                    <div class="d-flex flex-row align-items-center justify-content-center">
+                        <span class="m-2">Valor: </span>               
+                        <span id="historical-transaction-value" class="text-start text-sm-center text-${this.applyColor} fw-bold"> ${transactionValue}</span>
+                    </div>
+                    
+                  </div>                                             
+                `);                
             }    
         
-        },  
+        }, 
+        setColor() {            
+            if(this.historyType === 'Despesa') {
+                this.applyColor = 'danger'               
+                return
+            }
+           
+            if(this.historyType === 'Receita'){
+                this.applyColor = 'success'
+            }
+        },
         newTransaction() {         
 
-            if(this.descriptionInput !== '' && this.typeInput === 'Receita' && this.valueInput !== '' && !isNaN(this.valueInput)) {   
+            if(this.descriptionInput !== '' && this.typeInput === 'Receita' && this.valueInput !== '' && this.valueInput > 0 && !isNaN(this.valueInput)) {   
                 this.financialIncome += parseInt(this.valueInput)
                 
                 this.storeData()
@@ -79,7 +104,7 @@ const app = {
                 return        
             }
             
-            if(this.descriptionInput !== '' && this.typeInput === 'Despesa' && this.valueInput !== '' && !isNaN(this.valueInput)) {  
+            if(this.descriptionInput !== '' && this.typeInput === 'Despesa' && this.valueInput !== '' && this.valueInput > 0 && !isNaN(this.valueInput)) {  
                 this.financialExpenses += parseInt(this.valueInput)
 
                 this.storeData()
@@ -116,11 +141,12 @@ const app = {
         showCurrentBalance() {
             const currentBalance = document.querySelector('#current-balance');
             const financialIncome = document.querySelector('#financial-income');
-            const financialExpenses = document.querySelector('#financial-expenses');
+            const financialExpenses = document.querySelector('#financial-expenses');       
 
             let amountString = this.amount;
             let financialExpensesString = this.financialExpenses;
-            let financialIncomeString = this.financialIncome    
+            let financialIncomeString = this.financialIncome;
+             
             
             const formatNumber = (value) => {
                 const options = {
@@ -134,7 +160,8 @@ const app = {
             
               currentBalance.textContent = formatNumber(amountString);
               financialIncome.textContent = formatNumber(financialIncomeString);
-              financialExpenses.textContent = formatNumber(financialExpensesString);
+              financialExpenses.textContent = formatNumber(financialExpensesString);  
+
         },
         reload() {
             setTimeout(() => {
@@ -142,12 +169,12 @@ const app = {
             }, 200); 
         },              
     },
-    mounted() {
+    mounted() {       
        this.getCurrentDateTime()
        this.setCurrentYear()
        this.getTransactionHistory()
        this.currentBalance()
-       this.showCurrentBalance()       
+       this.showCurrentBalance()     
     }
 }
 
